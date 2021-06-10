ea0b5c17661af4f19e827e03b8ed3fe8f6d183d5
Antonette Caldwell <aacaldwell42@outlook.com>
2021-06-10 17:04:36 -0500

    Fixed redundancy

--- src/kairos/include/sys/types.h
+++ src/kairos/include/sys/types.h
@@ -4,20 +4,16 @@
 #define __need_size_t
 #include <stddef.h>
 
+/**
+ *  Defining types for the libc
+ **/
+
 #include <__typedef_blksize_t.h>
 #include <__typedef_nlink_t.h>
 #include <__typedef_off_t.h>
 #include <__typedef_ksize_t.h>
 #include <__typedef_suseconds_t.h>
 
-/**
- *  Defining types for the libc
- **/
-
-typedef long long blksize_t;
-typedef long long nlink_t;
-typedef long long off_t;
-typedef unsigned long ksize_t;
 typedef unsigned short kushort;
 typedef unsigned short kuchar;
 typedef ksize_t pde_t;
