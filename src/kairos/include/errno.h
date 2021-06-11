df4590e9f9a66c8e62d28445420ff79f8d3a4257
Antonette Caldwell <aacaldwell42@outlook.com>
2021-06-10 19:54:02 -0500

    Refactored errno

--- src/kairos/include/errno.h
+++ src/kairos/include/errno.h
@@ -1,27 +1,6 @@
-#ifndef __kairos_libc_errno_h
-#define __kairos_libc_errno_h
+#ifndef __kairos_errno_h
+#define __kairos_errno_h
 
-#include "sys/cdefs.h"
+#include <__errno.h>
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-#ifdef KAIROS_THREAD_MODEL_SINGLE
-    extern int errno;
-#else
-#ifdef __cplusplus
-extern thread_local int errno;
-#else
-extern _Thread_local int errno;
-#endif
-#endif
-
-#define errno errno
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+#endif // __kairos_errno_h
