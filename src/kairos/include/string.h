b00028c0c98026ce3ebb576ab78f1f5abe991d13
Antonette Caldwell <aacaldwell42@outlook.com>
2021-06-10 19:54:43 -0500

    Refactored string

--- src/kairos/include/string.h
+++ src/kairos/include/string.h
@@ -1,21 +1,6 @@
-#ifndef __kairos_libc_string_h
-#define __kairos_libc_string_h
+#ifndef __kairos_string_h
+#define __kairos_string_h
 
-#define __need_size_t
-#define __need_kchar_t
-#define __need_NULL
+#include <__functions_memcpy.h>
 
-#include "sys/types.h"
-#include "sys/cdefs.h"
-
-#include <stddef.h>
-
-__BEGIN_C_CODE
-
-void *memcpy(void *, const void *, ksize_t);
-void *memmove(void *, const void *, ksize_t);
-void *memset(void *, int, ksize_t);
-
-__END_C_CODE
-
-#endif // __kairos_libc_string_h
+#endif // __kairos_string_h
